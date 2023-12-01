627c89c9b7adee1d37910a91614880b18614708a
zheng xue <2395035@edu.vaniercollege.qc.ca>
2023-12-01 13:42:06 -0500

    App Dev with Android
    IntelliJ IDEA for Android App

---
+++ TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.tipcalculator;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView amountTextView;
+
+    TextView tipTextView;
+
+    TextView totalAmountTextView;
+
+    SeekBar seekBar;
+
+    TextView tipPercentTextView;
+
+    NumberFormat percentFormat=NumberFormat.getPercentInstance();
+
+    NumberFormat currencyFormat=NumberFormat.getCurrencyInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//inflating the xml
+
+        //reference to the widgets
+         amountTextView=findViewById(R.id.amountEditText);
+        tipTextView=findViewById(R.id.calculatedTipTextView);
+         totalAmountTextView=findViewById(R.id.calculatedTotalAmount);
+        seekBar=findViewById(R.id.percentSeekBar);
+        tipPercentTextView=findViewById(R.id.tipPercentTextView);
+        //set up the listeners
+        amountTextView.addTextChangedListener(amountTextViewWatcher);
+        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+    }
+    SeekBar.OnSeekBarChangeListener seekBarChangeListener=new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            double percentage=(double)(seekBar.getProgress())/100;
+            tipPercentTextView.setText(percentFormat.format(percentage));
+            calculate();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+    TextWatcher amountTextViewWatcher=new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+    private void calculate() {
+        String amountString=amountTextView.getText().toString();
+        //CharSequence amountString=amountTextView.getText();amountString==null
+        double amount=0.0;
+        if(amountString.isEmpty()){
+            tipTextView.setText(currencyFormat.format(0.0));
+            totalAmountTextView.setText(currencyFormat.format(0.0));
+        }else
+        {amount= Double.parseDouble(amountTextView.getText().toString());
+        int tipPercentage=seekBar.getProgress();
+        double tip=amount*tipPercentage/100;
+        double totalAmount=amount +tip;
+        tipTextView.setText(String.valueOf(currencyFormat.format(tip)));
+        totalAmountTextView.setText(currencyFormat.format(totalAmount));}
+
+    }
+}
